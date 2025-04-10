2525ca0f5c966947f9a47da2964df9ea54675f7e
Aryan <ap890383@gmail.com>
2025-04-10 18:43:49 +0530

    fixed routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -13,7 +13,7 @@ router.post("/login",Login)
 
 router.post("/logout",Logout)
 
-router.put("update-profile",protectRoute,updateProfile)
+router.put("/update-profile",protectRoute,updateProfile)
 
 router.get('/check',protectRoute,checkAuth)
 
