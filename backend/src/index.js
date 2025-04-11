c31248c6361e170deb02943580044cd7e759a7b8
Aryan <ap890383@gmail.com>
2025-04-11 09:56:47 +0530

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ app.use(cors({
   credentials:true
 }))
 
-app.use(express.json())
+app.use(express.json({limit : "10mb"}))
 app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use("/api/message",messageRoutes)
