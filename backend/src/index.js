c243f04ad144a39bb2ac04fff008b8ae7bfd4ac6
Aryan <ap890383@gmail.com>
2025-04-12 10:24:49 +0530

    gitignore done

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import {app,server} from "./lib/socket.js"
+import path from "path"
 
 dotenv.config();
 
@@ -13,7 +14,7 @@ app.use(cors({
   origin:"http://localhost:5173",
   credentials:true
 }))
-
+const __dirname = path.resolve()
 app.use(express.json({limit : "10mb"}))
 app.use(cookieParser())
 app.use('/api/auth',authRoutes)
